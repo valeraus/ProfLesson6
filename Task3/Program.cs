9ef85b32bd9a88ab01a9f54093f32f821ac517b2
Valera <vlaus0@gmail.com>
2023-04-07 23:08:48 +0300

    Добавьте файлы проекта.

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.Load("Task_2");
+
+            dynamic instance = Activator.CreateInstance(assembly.GetType("Task_2.Temperature"));
+
+            Console.WriteLine("15 °C по °F равно " + instance.Fahrenheit(15m));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_2/Temperature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class Temperature
+    {
+        public decimal Fahrenheit(decimal temperature)
+        {
+            return temperature * 9 / 5 + 32;
+        }
+    }
+}
